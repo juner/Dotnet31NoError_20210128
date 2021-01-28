a8cf23a5981f4b36371504761328d1f7738f98cc
juner <juner@juner.net>
2021-01-28 16:51:46 +0900

    first commit.

---
+++ Dotnet31NoError/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dotnet31NoError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = new object();
+            if (value is not object)
+                Console.WriteLine("hello!");
+            Console.WriteLine("world!");
+        }
+    }
+}
